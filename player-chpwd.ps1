e2a638d03d10cddb4b804a12b634bbae9499fab2
Pete Jansz <peterjansz@gmail.com>
2018-04-30 15:32:58 -0700

    Parameterized!

--- player-chpwd.ps1
+++ player-chpwd.ps1
@@ -1,28 +1,66 @@
+param
+(
+    [string]$hostname,
+    [int] $port = 80,
+    [string]$username,
+    [string]$password,
+    [int] $changeCount = 1,
+    [switch]$quiet = $false,
+    [switch]$go = $false,
+    [switch]$help,
+    [switch]$h
+)
+
 $ErrorActionPreference = "stop"
 Set-StrictMode -Version Latest
-
-$hostname = 'cadev1'
-$port = 443
-$username = 'test50@yopmail.com'
-# pdplayer -hostname player.calottery.com -port 443 -username test50@yopmail.com -chpwd RegTest6100 -newpwd Password123
-
-for ($i=1; $i -le 200; $i++)
+$ScriptName = $MyInvocation.MyCommand.Name
+$ScriptDir = Split-Path $MyInvocation.MyCommand.Path
+function showHelp()
 {
-    $newpwd = $null
-    $chpwd = $null
+    Write-Host "USAGE: ${ScriptName} [options] -hostname <hostname> -username <name> -password <password>"
+    Write-Host "Options:"
+    Write-Host "  -port <port>                      default = 80"
+    Write-Host "  -changeCount <int>                default = 1"
+    Write-Host "  -go                               Default print what would be done"
+    Write-Host "  -quiet"
 
-    if ($i % 2)
-    {
-        $newpwd = "welcome02"
-        $chpwd = "welcome01"
-    }
-    else
-    {
-        $newpwd = "welcome01"
-        $chpwd = "welcome02"
-    }
+    exit 1
+}
 
-    $theCall = "pdplayer -hostname $hostname -username $username -chpwd $chpwd -newpwd $newpwd"
-    "{0}: `t{1}" -f $i, $theCall
-    pdplayer -hostname $hostname -username $username -chpwd $chpwd -newpwd $newpwd
+if ($h -or $help) {showHelp}
+if ( -not($hostname) ) {showHelp}
+if ( -not($username) ) {showHelp}
+if ( -not($password) ) {showHelp}
+
+function restorePassword ($curpwd, $restorepwd)
+{
+    $theCall = "-hostname $hostname -port $port -username $username -chpwd $curpwd -newpwd $restorepwd"
+    $msg = "{0}: `t{1}" -f 'restorePassrword', $theCall
+
+    if (-not ($quiet)) {Write-Output $msg}
+
+    if ($go)
+    {
+        pdplayer -hostname $hostname -port $port -username $username -chpwd $curpwd -newpwd $restorepwd
+    }
+}
+
+function changePassword ($curpwd, $newpwd)
+{
+    $theCall = "-hostname $hostname -port $port -username $username -chpwd $curpwd -newpwd $newpwd"
+    $msg = "  {0}: `t{1}" -f 'changePassword', $theCall
+    if (-not ($quiet)) {Write-Output $msg}
+
+    if ($go)
+    {
+        pdplayer -hostname $hostname -port $port -username $username -chpwd $curpwd -newpwd $newpwd
+    }
+}
+
+$newPassword = "Newpassword1"
+
+for ($i = 1; $i -le ($changeCount) ; $i++)
+{
+    changePassword $password $newPassword
+    restorePassword $newPassword  $password
 }
