0a065b1ff8311d8a7a100cd882670e0a17779d93
Pete Jansz <peterjansz@gmail.com>
2018-11-07 15:40:51 -0800

    Fixed to login, perform aync GET on several player/self apis

--- async-pd-player-self.js
+++ async-pd-player-self.js
@@ -1,95 +1,192 @@
 /*
-    Axios concurrent REST calls, wait for all responses, promises
+    Axios aysynchronous/concurrent REST calls, wait for all responses, promises
+    Pete Jansz
 */
 
 const modulesPath = '/usr/share/node_modules/'
 const axios = require( modulesPath + 'axios' )
 var program = require( modulesPath + 'commander' )
+var igtCas = require( modulesPath + 'pete-lib/igt-cas' )
 
 program
     .version( '0.0.1' )
     .description( 'NodeJS CLI to login, call some PD APIs' )
     .usage( '-h <host> -u <username> -p <password>' )
+    .option( '-c, --count [count]', 'Repeat count times' )
     .option( '-h, --hostname <hostname>', 'Hostname' )
-    .option( '-t, --token <token>', 'OAuth token' )
+    .option( '-u, --username [username]', 'Username' )
+    .option( '-p, --password [password]', 'Password' )
     .option( '-q, --quiet', 'Shhhh' )
     .parse( process.argv )
 
 process.exitCode = 1
 
-if ( !program.hostname && !program.token )
+if ( !program.hostname && !program.username || !program.password )
 {
     program.help()
 }
 
-var host = program.hostname
-var oauthToken = program.token
-var axiosCadev1Players = createAxiosInstance( host, oauthToken )
+var axiosInstance
+main()
+
+async function main()
+{
+    try
+    {
+        var reqData = igtCas.createLoginRequest( program.hostname )
+        reqData.siteId = igtCas.getSiteId()
+
+        axiosInstance = createAxiosInstance( program.hostname )
+
+        // Login for the authCode:
+        reqData.resourceOwnerCredentials = { USERNAME: program.username, PASSWORD: program.password }
+        var promisedLoginAuthCodeResponse = await axiosInstance.post( '/api/v1/oauth/login', reqData )
+
+        var oauthTokenRequest =
+        {
+            authCode: promisedLoginAuthCodeResponse.data[0].authCode,
+            clientId: reqData.clientId,
+            siteId: reqData.siteId
+        }
+
+        // Submit authCode for the oauthToken:
+        var promisedTokenResponse = await axiosInstance.post( '/api/v1/oauth/self/tokens', oauthTokenRequest )
+        var mobileToken = promisedTokenResponse.data[0].token
+        var pwsToken = promisedTokenResponse.data[1].token
+        var oauthToken = program.hostname.match( /mobile/ ) ? mobileToken : pwsToken
+
+        axiosInstance.defaults.headers.common['Authorization'] = "OAuth " + oauthToken
+
+        var output = {}
+        var count = program.count ? program.count : 1
+        for ( var i = 0; i < count; i++ )
+        {
+            axios.all
+                (
+                [
+                    getAttributes(),
+                    getCommunicationPreferences(),
+                    getNotifications(),
+                    getNotificationPreferences(),
+                    getProfile(),
+                    getPersonalInfo()
+                ]
+                )
+                .then( axios.spread( function
+                    (
+                        attribs, commPreferences, notifications, notifPreferences, profile, persInfo
+                    )
+                {
+                    output.attributes = attribs.data
+                    output.communicationPreferences = commPreferences.data
+                    output.notifications = notifications.data
+                    output.notifPreferences = notifPreferences.data
+                    output.profile = profile.data
+                    output.personalInfo = persInfo.data
+
+                    if ( !program.quiet ) { console.log( JSON.stringify( output ) ) }
+                } )
+                )
+        }
+
+    process.exitCode = 0
+    }
+    catch ( error )
+    {
+        axiosErrorHandler( error )
+    }
+}
 
 function createAxiosInstance( host, oauthToken )
 {
-    var proto = host.match( /dev/i ) ? 'http' : 'https'
+    var proto = 'https'
 
-    return axios.create( {
-        baseURL: proto + '://' + host + '/api/v1/players/self',
-        headers: { 'X-EX-SYSTEM-ID': '8', 'X-CHANNEL-ID': '2', 'X-SITE-ID': '35', "Authorization": "OAuth " + oauthToken }
-    } )
+    if ( host.match( /dev/ ) )
+    {
+        proto = 'http'
+    }
+
+    var defaults =
+    {
+        baseURL: proto + '://' + host,
+        headers: igtCas.createHeaders( host )
+    }
+    if ( oauthToken ) { defaults.headers['Authorization'] = 'OAuth ' + oauthToken }
+
+    return axios.create( defaults )
+}
+
+async function loggedInAxios( host, credentials )
+{
+    var reqData = igtCas.createLoginRequest( host )
+    reqData.resourceOwnerCredentials = { USERNAME: credentials.username, PASSWORD: credentials.password }
+    var axiosInstance = createAxiosInstance( host )
+
+    // Get OAuth authCode
+    axiosInstance.post( '/api/v1/oauth/login', reqData )
+    var resultPromise = await oauthAxiosInstance.post( '/api/v1/oauth/login', oauthLoginRequest )
+    var oauthTokenRequest =
+    {
+        authCode: resultPromise.data[0].authCode,
+        clientId: clientId,
+        siteId: siteId
+    }
+
+    return axiosInstance.post( '/api/v1/oauth/self/tokens', oauthTokenRequest )
 }
 
 function getAttributes()
 {
-    return axiosCadev1Players.get( '/attributes' )
+    return axiosInstance.get( '/api/v1/players/self/attributes' )
+}
+
+function axiosErrorHandler( error )
+{
+    if ( error.response )
+    {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.error( error.response.data )
+        console.error( error.response.status )
+        console.error( error.response.headers )
+    }
+    else if ( error.request )
+    {
+        // The request was made but no response was received
+        // error.request is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.error( error.request )
+    }
+    else
+    {
+        // Something happened in setting up the request that triggered an Error
+        console.error( 'Error', error.message )
+    }
+    console.error( error.config )
 }
 
 function getCommunicationPreferences()
 {
-    return axiosCadev1Players.get( '/communication-preferences' )
+    return axiosInstance.get( '/api/v1/players/self/communication-preferences' )
 }
 
 function getNotifications()
 {
-    return axiosCadev1Players.get( '/notifications' )
+    return axiosInstance.get( '/api/v1/players/self/notifications' )
 }
 
 function getNotificationPreferences()
 {
-    return axiosCadev1Players.get( '/notifications-preferences' )
+    return axiosInstance.get( '/api/v1/players/self/notifications-preferences' )
 }
 
 function getProfile()
 {
-    return axiosCadev1Players.get( '/profile' )
+    return axiosInstance.get( '/api/v1/players/self/profile' )
 }
 
 function getPersonalInfo()
 {
-    return axiosCadev1Players.get( '/personal-info' )
+    return axiosInstance.get( '/api/v1/players/self/personal-info' )
 }
 
-var output = {}
-
-axios.all
-    (
-    [getAttributes(),
-    getCommunicationPreferences(),
-    getNotifications(),
-    getNotificationPreferences(),
-    getProfile(),
-    getPersonalInfo()]
-    )
-    .then( axios.spread( function ( attribs, communicationPreferences, notifications, notifPreferences, profile, persInfo )
-    {
-        output.attributes = attribs.data
-        output.communicationPreferences = communicationPreferences.data
-        output.notifications = notifications.data
-        output.notifPreferences = notifPreferences.data
-        output.profile = profile.data
-        output.personalInfo = persInfo.data
-        process.exitCode = 0
-
-        if ( !program.quiet ) { console.log( JSON.stringify( output ) ) }
-    } )
-    ).catch( function ( error )
-    {
-        console.error( error )
-    } )
