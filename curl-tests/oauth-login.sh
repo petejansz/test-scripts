658f0a537f0a996bc65c5042e9d945c8b5ce3f2e
Pete Jansz <peterjansz@gmail.com>
2018-11-15 09:08:19 -0800

    Support direct call to sec-gateway

--- curl-tests/oauth-login.sh
+++ curl-tests/oauth-login.sh
@@ -5,6 +5,8 @@
 
 SCRIPT=$(basename $0)
 HOST=
+PORT=
+SEC_GW_PORT=8280
 USERNAME=
 PASSWORD=
 CA_SITE_ID=35
@@ -28,17 +30,16 @@ TOKEN_CUT_FIELD_NR=$PWS_TOKEN_CUT_FLD_NR
 function help()
 {
   echo "OAuth login to CA Player Direct, write oauth-token to stdout"              >&2
+  echo "    port default=sec-gateway port 8280 when host =~ IP-address"            >&2
   echo ""                                                                          >&2
-  echo "USAGE: $SCRIPT [options] -h <hostname> -u <username> -p <password>"        >&2
+  echo "USAGE: $SCRIPT [options] -h <host> -u <username> -p <password>"            >&2
   echo "  options"                                                                 >&2
-  echo "  -h | --host     <host>"                                                  >&2
-  echo "  -u | --username <username>"                                              >&2
-  echo "  -p | --password <password>"                                              >&2
+  echo "       --port     <port>"                                                  >&2
   echo '  -?   --help'                                                             >&2
 }
 
 # options parser:
-OPTS=$(getopt -o h:u:p: --long host:,username:,password:,help -n 'parse-options' -- "$@")
+OPTS=$(getopt -o h:u:p: --long host:,username:,password:,port:,help -n 'parse-options' -- "$@")
 if [ $? != 0 ]; then
   help
   exit 1
@@ -48,6 +49,7 @@ eval set -- "$OPTS"
 while true; do
   case "$1" in
       -h | --host     ) HOST="$2";     shift; shift ;;
+           --port     ) PORT="$2";     shift; shift ;;
       -p | --password ) PASSWORD="$2"; shift; shift ;;
       -u | --username ) USERNAME="$2"; shift; shift ;;
            --help     ) HELP=true;     shift ;;
@@ -67,16 +69,20 @@ if [[ $HOST =~ "mobile" ]]; then
     TOKEN_CUT_FIELD_NR=$MOB_TOKEN_CUT_FLD_NR
 fi
 
-HOSTNAME=$HOST
-PROTO=http
-if [[ "$HOSTNAME" =~ '.com' ]]; then
-    PROTO=https
+if [[ "${HOST}" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ && -z "$PORT" ]]; then
+    BASE_URI="http://${HOST}:${SEC_GW_PORT}/california-gateway/api/v1/oauth"
+elif [[ "${HOST}" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ && "$PORT" ]]; then
+    BASE_URI="http://${HOST}:${PORT}/california-gateway/api/v1/oauth"
+elif [[ "${HOST}" =~ '.com' ]]; then
+    BASE_URI="https://${HOST}/api/v1/oauth"
+else
+    BASE_URI="http://${HOST}/api/v1/oauth"
 fi
 
 RESOURCE_CREDS="\"resourceOwnerCredentials\": {\"USERNAME\":\"${USERNAME}\", \"PASSWORD\":\"${PASSWORD}\"}"
 
-AUTH_CODE=$(curl -ksX POST \
-  "${PROTO}://${HOSTNAME}/api/v1/oauth/login"       \
+RESP=$(curl -ksX POST \
+  "${BASE_URI}/login"       \
   -H 'cache-control: no-cache'                      \
   -H 'content-type: application/json'               \
   -H "x-site-id: $siteId"                           \
@@ -84,11 +90,23 @@ AUTH_CODE=$(curl -ksX POST \
   -H "x-channel-id: $channelId"                     \
   -H "x-esa-api-key: ${esaApiKey}"                  \
   -H "x-clientip: $(hostname)"                      \
-  -d "{ \"siteId\":\"${siteId}\", \"clientId\":\"${clientId}\", ${RESOURCE_CREDS} }" \
-  | cut -d: -f 6 | cut -d, -f 1 | sed 's/"//g')
+  -d "{ \"siteId\":\"${siteId}\", \"clientId\":\"${clientId}\", ${RESOURCE_CREDS} }")
+
+if [[ $? > 0 ]]; then
+    echo "oauth/login failed" >&2
+    echo "${RESP}"            >&2
+    exit 1
+fi
+
+AUTH_CODE=$(echo ${RESP} | cut -d: -f 6 | cut -d, -f 1 | sed 's/"//g')
+
+if [[ -z "${AUTH_CODE}" ]]; then
+    echo "oauth/login failed, can't parse for authCode" >&2
+    exit 1
+fi
 
 TOKENS_RESP=$(curl -ksX POST \
-  "${PROTO}://${HOSTNAME}/api/v1/oauth/self/tokens" \
+  "${BASE_URI}/self/tokens" \
   -H 'cache-control: no-cache'                      \
   -H 'content-type: application/json'               \
   -H "x-site-id: $siteId"                           \
