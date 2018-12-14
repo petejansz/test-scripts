d74116ef878481fe0869e646af5829b496ae5ef3
Pete Jansz <peterjansz@gmail.com>
2018-12-14 08:56:50 -0800

    Add finishing touches

--- player-chpwd.ps1
+++ player-chpwd.ps1
@@ -1,12 +1,16 @@
+<#
+    Command-line script to automate, repeatedly a PD player changing thier password. Original password is restored.
+    Author: Pete Jansz, IGT, June 2017
+#>
+
 param
 (
     [string]$hostname,
     [int] $port = 80,
     [string]$username,
     [string]$password,
-    [int] $changeCount = 1,
+    [int] $count = 1,
     [switch]$quiet = $false,
-    [switch]$go = $false,
     [switch]$help,
     [switch]$h
 )
@@ -17,11 +21,12 @@ $ScriptName = $MyInvocation.MyCommand.Name
 $ScriptDir = Split-Path $MyInvocation.MyCommand.Path
 function showHelp()
 {
+    Write-Host "Automate, repeatedly a PD player changing thier password."
+    Write-Host "The original password is restored.`n"
     Write-Host "USAGE: ${ScriptName} [options] -hostname <hostname> -username <name> -password <password>"
     Write-Host "Options:"
-    Write-Host "  -port <port>                      default = 80"
-    Write-Host "  -changeCount <int>                default = 1"
-    Write-Host "  -go                               Default print what would be done"
+    Write-Host "  -port <port>    default = 80"
+    Write-Host "  -count <int>    default = 1"
     Write-Host "  -quiet"
 
     exit 1
@@ -39,10 +44,7 @@ function restorePassword ($curpwd, $restorepwd)
 
     if (-not ($quiet)) {Write-Output $msg}
 
-    if ($go)
-    {
-        pdplayer -hostname $hostname -port $port -username $username -chpwd $curpwd -newpwd $restorepwd
-    }
+    pdplayer -hostname $hostname -port $port -username $username -chpwd $curpwd -newpwd $restorepwd
 }
 
 function changePassword ($curpwd, $newpwd)
@@ -51,15 +53,12 @@ function changePassword ($curpwd, $newpwd)
     $msg = "  {0}: `t{1}" -f 'changePassword', $theCall
     if (-not ($quiet)) {Write-Output $msg}
 
-    if ($go)
-    {
-        pdplayer -hostname $hostname -port $port -username $username -chpwd $curpwd -newpwd $newpwd
-    }
+    pdplayer -hostname $hostname -port $port -username $username -chpwd $curpwd -newpwd $newpwd
 }
 
 $newPassword = "Newpassword1"
 
-for ($i = 1; $i -le ($changeCount) ; $i++)
+for ($i = 1; $i -le $count; $i++)
 {
     changePassword $password $newPassword
     restorePassword $newPassword  $password
