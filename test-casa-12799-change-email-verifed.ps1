f1f49c28b62f478c58437714a9e0103e5a07b266
Pete Jansz <peterjansz@gmail.com>
2019-04-24 09:09:04 -0700

    Make flexible

--- test-casa-12799-change-email-verifed.ps1
+++ test-casa-12799-change-email-verifed.ps1
@@ -4,7 +4,10 @@ param
 (
     [string]$envname = "ca-dev-pd",
     [string]$p = "Password1",
-    #[int]$playerid,
+    [switch]$current,
+    [switch]$activate,
+    [switch]$preactivate,
+    [switch]$suspend,
     [switch]$quiet = $false,
     [string]$u,
     [switch]$help
@@ -17,44 +20,72 @@ $ScriptDir = Split-Path $MyInvocation.MyCommand.Path
 function showHelp()
 {
     Write-Host "Test PD player activated->suspended->preactive"
-    Write-Host "USAGE: ${ScriptName} [options] -envname <env-name (default=${envname}) > -u <username> -p <password> "
+    Write-Host "USAGE: ${ScriptName} [options] -envname <env-name (default=${envname})> -u <username> [-p <password>] "
     Write-Host "Options:"
-
+    Write-Host "  -activate"
+    Write-Host "  -current"
+    Write-Host "  -preactivate"
+    Write-Host "  -suspend"
     exit 1
 }
 
+function serviceState([int] $serviceStatus)
+{
+    $state = ""
+    if     ($serviceStatus -eq 1) {$state = "PRE"}
+    elseif ($serviceStatus -eq 2) {$state = "ACT"}
+    elseif ($serviceStatus -eq 3) {$state = "SUS"}
+
+    return $state
+}
 function playerServiceToStr($services)
 {
     $str = "playerPortalServiceStatus: {0}, secondChanceServiceStatus: {1}, emailVerified: {2}" -f `
-        $services.playerPortalServiceStatus, $services.secondChanceServiceStatus, $services.emailVerified
+    (serviceState $services.playerPortalServiceStatus), (serviceState $services.secondChanceServiceStatus), $services.emailVerified
     return $str
 }
 
-function setAccountToActivatedState()
+function currentState()
 {
-    Write-Host "Setting account to verified, activated state ... "  -NoNewLine
-    $adminHost = $envname.split('-')[1]
-    $playerid = (pd2-admin --host $adminHost --api search --email $u | ConvertFrom-Json).playerid
-    pdplayer -h (env-alias.js -a $envname) -u $u -verify $playerid | Out-Null
 
     $playerServices = pdplayer -h (env-alias.js -a $envname) -u $u -p $p -getattributes | ConvertFrom-Json
     Write-Host  (playerServiceToStr $playerServices)
+}
+function activate()
+{
+    Write-Host "Setting account to verified, activated state ... "  #-NoNewLine
+    $adminHost = $envname.split('-')[1]
+    $playerid = (pd2-admin --host $adminHost --api search --email $u | ConvertFrom-Json).playerid
+    $code = get-activation-code.ps1 -u $u
+    pdplayer -h (env-alias.js -a $envname) -u $u -activate $code | Out-Null
+    currentState
 }
 
 function suspendAccount
 {
-    Write-Host "Suspending account ... " -NoNewLine
+    Write-Host "Suspending account ... " #-NoNewLine
     (pdplayer -h (env-alias.js -a $envname) -u $u -p $p -lock 'Lock it!').statuscode
-    $playerServices = pdplayer -h (env-alias.js -a $envname) -u $u -p $p -getattributes | ConvertFrom-Json
-    Write-Host  (playerServiceToStr $playerServices)
+    currentState
 }
 
 function preactivateAccount
 {
-    Write-Host "PREACTIVating account ... " -NoNewLine
+    Write-Host "Preactivating account ... "<#
+    .SYNOPSIS
+
+
+    .DESCRIPTION
+    Long description
+
+    .EXAMPLE
+    An example
+
+    .NOTES
+    General notes
+    #>
+    -NoNewLine
     (pdplayer -h (env-alias.js -a $envname) -u $u -p $p -unlock 'Unlock it!').statuscode
-    $playerServices = pdplayer -h (env-alias.js -a $envname) -u $u -p $p -getattributes | ConvertFrom-Json
-    Write-Host  (playerServiceToStr $playerServices)
+    currentState
 }
 
 if (     $help) {showHelp}
@@ -62,11 +93,7 @@ if ( -not($envname) ) {showHelp}
 if ( -not($u) ) {showHelp}
 if ( -not($p) ) {showHelp}
 
-Write-Host  "Current state ... " -NoNewLine
-$playerServices = pdplayer -h (env-alias.js -a $envname) -u $u -p $p -getattributes | ConvertFrom-Json
-Write-Host  (playerServiceToStr $playerServices)
-
-if ($playerServices.emailVerified -eq $false)
-{ setAccountToActivatedState }
-suspendAccount
-preactivateAccount
+if ($current)       {currentState}
+if ($activate)      { activate }
+if ($suspend)       {suspendAccount}
+if ($preactivate)   {preactivateAccount}
