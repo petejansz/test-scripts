5baa893e03861b0f5faa5c3b80ed1542782ca21d
Pete Jansz <peterjansz@gmail.com>
2020-06-29 11:06:54 -0700

    Add call to forgotten-password API function.

--- curl-tests/pd-player-self.sh
+++ curl-tests/pd-player-self.sh
@@ -5,7 +5,7 @@
 #     'attributes' 'personal-info' 'profile' 'notifications-preferences' 'communication-preferences
 #   Pete Jansz, IGT, 2018-11-10
 
-EXECUTING_DIR=$( dirname $(readlink -f $0 ))
+EXECUTING_DIR=$( dirname $(readlink -f $(which $0 )))
 . $EXECUTING_DIR/pd-ca-lib.sh
 
 SCRIPT=$(basename $0)
@@ -14,6 +14,7 @@ USERNAME=
 PASSWORD=
 OAUTH=
 AVAILABLE=
+FORGOTTEN_PASSWORD=
 
 # Defaults:
 QUIET=false
@@ -31,7 +32,8 @@ function help()
   echo "Login, get oauth token, make GET calls to API-names:"                                     >&2
   echo "  $ALL_API_NAMES"                                                                         >&2
   echo                                                                                            >&2
-  echo "USAGE: $(basename $0) [options] -h <host> -u <username> -p <password> | -o <oauth token> | --available <username>" >&2
+  echo "USAGE: $(basename $0) [options] -h <host> -u <username> -p <password> | -o <oauth token>" >&2
+  echo "   | --available <username> | --forgot[_pwd] <username>"                                  >&2
   echo "  options"                                                                                >&2
   echo "  -h | --host     <host>  To talk directly to sec-gateway use IP-address as host"         >&2
   echo "       --port     <port>  When host is an IP-address, port is set to sec-gateway 8280"    >&2
@@ -50,7 +52,7 @@ function help()
 }
 
 # options parser:
-OPTS=$(getopt -o c:h:w:o:u:p:qv --long apis:,count:,wait:,host:,oauth:,username:,password:,port:,available:,help,siteid:,quiet,verbose -n 'parse-options' -- "$@")
+OPTS=$(getopt -o c:h:w:o:u:p:qv --long apis:,count:,wait:,host:,oauth:,username:,password:,port:,available:,forgot_pwd:,help,siteid:,quiet,verbose -n 'parse-options' -- "$@")
 
 if [ $? != 0 ]; then
   help
@@ -64,6 +66,7 @@ while true; do
            --port     ) PORT="$2";      shift; shift ;;
       -c | --count    ) COUNT="$2";     shift; shift ;;
            --available ) AVAILABLE="$2" shift; shift ;;
+           --forgot_pwd ) FORGOTTEN_PASSWORD="$2" shift; shift ;;
       -o | --oauth    ) OAUTH="$2";     shift; shift ;;
       -p | --password ) PASSWORD="$2";  shift; shift ;;
       -u | --username ) USERNAME="$2";  shift; shift ;;
@@ -82,7 +85,7 @@ if [[ "$HELP" == 'true' || -z "$HOST" ]]; then
   exit 1
 fi
 
-if [[ -z "$OAUTH" && -z "$USERNAME" && -z "$PASSWORD"  && -z "$AVAILABLE" ]]; then
+if [[ -z "$OAUTH" && -z "$USERNAME" && -z "$PASSWORD"  && -z "$AVAILABLE" && -z "$FORGOTTEN_PASSWORD" ]]; then
   help
   exit 1
 fi
@@ -92,7 +95,7 @@ if [[ $HOST =~ "mobile" ]]; then
     clientId=$CA_MOBILE_CLIENT_ID
 fi
 
-if [[ -z "$API_NAMES" && -z "$AVAILABLE" ]]; then
+if [[ -z "$API_NAMES" && -z "$AVAILABLE" && -z "$FORGOTTEN_PASSWORD" ]]; then
     API_NAMES=$ALL_API_NAMES
 fi
 
@@ -104,8 +107,24 @@ function is_available()
       local CURL_OPTS=$DEFAULT_CURL_OPTS
     fi
 
-    if [[ -z "$ESA_API_KEY" ]]; then
-      local ESA_API_KEY=$DEFAULT_ESA_API_KEY
+    if [[ $VERBOSE == 'true' ]]; then
+        CURL_OPTS="-v ${CURL_OPTS}"
+    fi
+
+    BASE_URI=$(create_base_uri $HOST $PORT)
+    RESP=$(curl -s "${BASE_URI}/api/v1/players/available/${AVAILABLE}" \
+      -H "x-ex-system-id: $CA_SYSTEM_ID" \
+      -H "x-channel-id: $channelId" )
+
+    echo $RESP
+}
+
+function forgotten_password()
+{
+    local CURL_OPTS=$1
+
+    if [[ -z "$CURL_OPTS" ]]; then
+      local CURL_OPTS=$DEFAULT_CURL_OPTS
     fi
 
     if [[ $VERBOSE == 'true' ]]; then
@@ -113,9 +132,11 @@ function is_available()
     fi
 
     BASE_URI=$(create_base_uri $HOST $PORT)
-    RESP=$(curl -s "${BASE_URI}/api/v1/players/available/${AVAILABLE}" \
+    RESP=$(curl $CURL_OPTS -X PUT "${BASE_URI}/api/v1/players/forgotten-password" \
       -H "x-ex-system-id: $CA_SYSTEM_ID"                \
-      -H "x-channel-id: $channelId" )
+      -H "x-channel-id: $channelId"                     \
+      -H 'content-type: application/json'               \
+      --data-raw "{\"emailAddress\" : \"${FORGOTTEN_PASSWORD}\"}" )
 
     echo $RESP
 }
@@ -183,7 +204,7 @@ function exec_players_self_apis()
     done
 }
 
-if [[ -z "$OAUTH" && -z "$AVAILABLE" ]]; then
+if [[ -z "$OAUTH" && -z "$AVAILABLE" && -z "$FORGOTTEN_PASSWORD" ]]; then
     OAUTH=$(pd_login $HOST $USERNAME $PASSWORD)
 fi
 
@@ -195,6 +216,18 @@ while [[ $COUNT -ne 0 ]]; do
   if [[ -n "$AVAILABLE" ]]; then
     RESP=$( is_available )
     echo $RESP
+  fi
+
+  if [[ -n "$FORGOTTEN_PASSWORD" ]]; then
+    RESP=$( forgotten_password )
+    if [[ $RESP != 204 ]]; then
+        echo "Failed: forgotten-password : $RESP"
+        exit 1
+    fi
+
+    if [[ "$QUIET" =~ 'false'  ]]; then
+        echo "Response code ${RESP}: forgotten-password"
+    fi
   fi
 
   let COUNT--
