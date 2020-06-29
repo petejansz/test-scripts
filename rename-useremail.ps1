ce06d1ab1345c39bd878ddf802264025d14bba1a
Pete Jansz <peterjansz@gmail.com>
2020-06-29 11:08:47 -0700

    Make more flexible.

--- rename-useremail.ps1
+++ rename-useremail.ps1
@@ -4,10 +4,15 @@
 
 param
 (
+    [int]$port = 80,
+    [string]$h, # hostname
+    [string]$hostname,
+    [string]$u,
     [string]$username,
-    [string]$password = "RegTest6100",
-    [switch]    $help,
-    [switch]    $h
+    [string]$newUsername,
+    [string]$p        = "Password1",
+    [string]$password = "Password1",
+    [switch]    $help
 )
 
 $ErrorActionPreference = "Stop"
@@ -18,24 +23,29 @@ $ScriptName = $MyInvocation.MyCommand.Name
 function showHelp()
 {
     Write-Output "Rename pdplayer username to newname"
-    Write-Output "USAGE: $ScriptName [option] -username <curUsername>"
+    Write-Output "USAGE: $ScriptName [option] -h[ostname] <hostname> -username <name> -newUsername <name>"
     Write-Output "  option"
-    Write-Output "      -password <password default=RegTest6100>"
+    Write-Output "  -p[assword] <password default=$password>"
+    Write-Host "    -port <int default=${port}>"
     exit 1
 }
 
-if ($h -or $help) {showHelp}
-if ( -not($username) ) {showHelp}
+if ($help) {showHelp}
+if (-not ($h -or $hostname)) { showHelp }
+if ( -not($u -or $username) ) { showHelp }
+if ( -not($newUsername) ) { showHelp }
+
+if ($h) { $hostname = $h }
+if ($p) { $password = $p }
+if ($u) { $username = $u }
 
 $curUsername = $username
-($base, $domain) = $curUsername.split('@')
-$theChange = $base.Chars($base.Length - 1)
-$newUsername = "{0}{1}@{2}" -f $base, $theChange, $domain
-
 Write-Output "Renaming $curUsername to $newUsername ..."
-$curUsernameJson = pdplayer.ps1 -hostname cadev1 -getpersonalinfo -username $curUsername -password $password
-$obj = $curUsernameJson | convertfrom-json
+$obj = (pdplayer.ps1 -hostname $hostname -getpersonalinfo -username $curUsername -password $password).Content | convertfrom-json
 $obj.emails[0].PERSONAL.address = $newUsername
 $filename = "$env:TEMP/ch-email.json"
 $obj | ConvertTo-Json | Out-file -encoding UTF8 -force $filename
-pdplayer.ps1 -hostname cadev1 -updatepersonalinfo $filename -username $curUsername -password $password | out-null
+pdplayer.ps1 -h $hostname -updatepersonalinfo $filename -username $curUsername -password $password | out-null
+Start-Sleep 2
+$sessionToken = pdplayer.ps1 -h $hostname -logintoken $newUsername -password $password
+Write-Output "$newUsername logged in, session token: $sessionToken"
